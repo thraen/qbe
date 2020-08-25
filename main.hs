5216a8c28d245c2ec614637865f180a0aefb84bc
thraen <johannes.thraen@gmail.com>
2020-08-25 23:08:41 +0200

    remove display module

--- main.hs
+++ main.hs
@@ -1,6 +1,8 @@
-import Graphics.UI.GLUT
 import Data.IORef
-import Display
+import Cube
+import Control.Monad
+
+import Graphics.UI.GLUT hiding (translate, scale, rotate)
 
 type Spin = (GLfloat, (GLfloat, GLfloat, GLfloat)) 
 
@@ -18,7 +20,6 @@ main = do
 
     spin_      <- newIORef (0.0, (0.0,0.0,0.0))
     lastMouse_ <- newIORef (Position 0 0)
-    t          <- newIORef (0.0)
 
     keyboardMouseCallback $= Just (keyboardMouse lastMouse_ normalizeWindowCoo_ spin_)
 
@@ -60,12 +61,15 @@ spinFromMouseMotion :: NormalMouseCoo -> NormalMouseCoo -> Spin
 spinFromMouseMotion (lx,ly) (x,y) = (d, v) where d = 5*dist (lx, ly) (x, y)
                                                  v = (ly-y, x-lx, 0.0)
     
+
+
 updateSpinFromMouseMotion :: IORef CoordinateNormalization -> IORef Position -> Position -> IORef Spin -> IO ()
 updateSpinFromMouseMotion normalize_ lastMouse_ mouse spin_ = do
         normalize <- get normalize_
         lastMouse <- get lastMouse_
         let nLastMouseCoo  = normalize lastMouse
         let nMouseCoo      = normalize mouse
+
 --         putStrLn $ show nMouseCoo ++ show nLastMouseCoo
 
         spin_ $=! spinFromMouseMotion nLastMouseCoo nMouseCoo
@@ -98,4 +102,49 @@ idle spin_ = do
 --     putStrLn "idle"
     postRedisplay Nothing
 
+
+-- points :: Int -> [(Float,Float,Float)]
+-- points n = [ (sin (2*pi*k/n'), cos (2*pi*k/n'), 0) | k <- [1..n'] ]
+--    where n' = fromIntegral n
+
+-- myPoints :: [(Float,Float,Float)]
+-- myPoints = [ (sin (2*pi*k/12), cos (2*pi*k/12), 0) | k <- [1..12] ]
+
+spinit :: Spin -> IO()
+spinit (dv, w) = do
+--     putStrLn $ "spinit " ++ show dv ++ " about " ++ show w
+    rotate dv w
+
+init__ = do
+    loadIdentity
+    scale 0.25
+
+-- display spin_ = preservingMatrix $ do
+display spin_ = do
+    clear [ColorBuffer, DepthBuffer]
+    spin <- get spin_
+    -- viewing angle
+
+    renderPrimitive Lines $ do
+        vertfromVec (0, 0, 0)
+        vertfromVec (snd spin)
+    spinit spin
+
+    orthonormal
+
+    t <- elapsedTime
+--     let fu = fromIntegral t
+    let fu = sin (0.0008* (fromIntegral t))
+
+--     forM_ cube_ (draw_thing cubelet 0.6 )
+    forM_ cube_ (draw_thing cubelet fu )
+
+    swapBuffers
+
+-- display = do 
+--   clear [ColorBuffer]
+--   renderPrimitive Points $
+--      mapM_ (\(x, y, z) -> vertex $ Vertex3 x y z) myPoints
+--   flush
+ 
 
